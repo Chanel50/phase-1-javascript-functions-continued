9ead04a22bf1bb47d440d9b3b46e2c4494ecc4d4
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-09 01:28:49 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  
+  const myFunction = function() {
+   
+  };
+  
+ 
+  const myAnonymousFunction = function() {
+  
+  };
+  
+  // Define a Function Using a Function Expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  
+  (function() {
+    
+  })();
+  
+  
+  function outerFunction() {
+    const outerVar = "I'm in outer function";
+    
+    function innerFunction() {
+      const innerVar = "I'm in inner function";
+      console.log(outerVar); 
+    }
+    
+    innerFunction();
+  }
+  
+  
+  function demoChain(name) {
+    const part1 = "hi";
+    return function() {
+      const part2 = "there";
+      console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+    };
+  }
+  
+  
+  function closureExample() {
+    const outerVar = "I'm from outer function";
+    
+    return function() {
+      console.log(outerVar); 
+    };
+  }
+  
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+ 
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("awesome")); 
+  
+ 
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+  };
+  
